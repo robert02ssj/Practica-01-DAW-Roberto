d403c32005ffe9d07438e05a0eadd72758c39b1d
Roberto Fernandez <byrobertofd7@gmail.com>
2025-10-03 12:29:50 +0000

    añazo script y php

---
+++ Scripts/install-lamp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+
+# Actualizamos repositorios
+ apt update
+ apt upgrade -y
+# Instalamos Apache
+ apt install apache2 -y
+# Instalamos PhP
+ apt install php libapache2-mod-php php-mysql -y
+# Instalamos MySQL
+ apt install mysql-server -y
+# Reiniciamos Apache
+ systemctl restart apache2
+ #copiamos nuestro archivo de prueba php a /var/www/html
+    cp ../php/index.php /var/www/html
